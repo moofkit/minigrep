6823f0b02cec730026e004a6b6fd9c024001c26c
Dmitriy Ivliev <rootkit89@gmail.com>
2022-02-12 20:51:40 +0200

    adds case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub fn run(input: Input) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(input.filename)?;
-    for line in search(&input.query, &contents) {
+    let results = if input.case_sensitive {
+        search(&input.query, &contents)
+    } else {
+        search_case_insensitive(&input.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
 
@@ -13,6 +19,7 @@ pub fn run(input: Input) -> Result<(), Box<dyn Error>> {
 pub struct Input {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Input {
@@ -22,8 +29,9 @@ impl Input {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err() || env::var("CASE_INSENSITIVE").unwrap() == "0";
 
-        Ok(Input { query, filename })
+        Ok(Input { query, filename, case_sensitive })
     }
 }
 
@@ -34,15 +42,29 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         .collect()
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
-        let query = "reary";
+    fn case_sensitive() {
+        let query = "to";
         let contents = fs::read_to_string("test.txt").unwrap();
-        assert_eq!(vec!["How dreary to be somebody!"], search(query, &contents));
+        assert_eq!(vec!["Are you nobody, too?", "How dreary to be somebody!"], search(query, &contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "i'm";
+        let contents = fs::read_to_string("test.txt").unwrap();
+        assert_eq!(vec!["I'm nobody! Who are you?"], search_case_insensitive(query, &contents));
     }
 
     #[test]
