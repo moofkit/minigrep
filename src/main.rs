714e0565a470cb6341061a84c25736d4e5732242
Dmitriy Ivliev <rootkit89@gmail.com>
2022-02-12 20:55:50 +0200

    changes output error messages to stderr

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input = Input::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
 
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(input) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
 
         process::exit(1);
     }
